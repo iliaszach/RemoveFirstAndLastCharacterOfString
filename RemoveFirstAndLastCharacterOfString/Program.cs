908cfd667a03678f0d389c404bf375d590f2c6aa
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-06-18 10:53:38 +0300

    Add project files.

---
+++ RemoveFirstAndLastCharacterOfString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveFirstAndLastCharacterOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Remove_char("ok");
+            Console.WriteLine(a);
+        }
+
+        public static string Remove_char(string s)
+        {
+            var a = s.Remove(0, 1).Remove(s.Length - 2);
+            return a;
+        }
+    }
+    }
+}
